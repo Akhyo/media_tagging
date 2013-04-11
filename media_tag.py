2005023e7e53d8a7520229066f3b631aba0f2202
Akhyo <null>
2013-04-11 16:47:59 +0200

    Added test datafile to file containing cli interface.

--- media_tag.py
+++ media_tag.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 import pickle
+import magic
+import filerecord
+
+datafile='filedata.test'
+
+loadfile = open(datafile,'rb') 
+pickle.load(loadfile)
+loadfile.close()
+
+#Do stuff
+
+dumpfile = open(datafile,'wb')  
+pickle.dump(filerecord.all_data, dumpfile)
+dumpfile.close()
+
